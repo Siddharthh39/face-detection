75ab035b6c17ff6a837f4d058f9575c3bcda2209
Siddharthh39 <megaberu81@gmail.com>
2024-08-23 12:39:45 +0530

    md ready

--- main.py
+++ main.py
@@ -19,3 +19,6 @@ while True:
         break
 
 video_cap.release
+
+class faceRecogination():
+    pass
---
+++ readme.md
@@ -0,0 +1,5 @@
+# To-do
+init the function for face detection
+
+# to solve
+read the one face
